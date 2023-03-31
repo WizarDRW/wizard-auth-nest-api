893fb0e782e07062829a4ea353e506fede61492d
WizarDRW <wizard1456@hotmail.com>
2023-03-31 15:59:06 +0300

    Claim module added in app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './controllers/users/users.module';
+import { ClaimsModule } from './controllers/claims/claims.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ClaimsModule],
   controllers: [AppController],
   providers: [AppService],
 })
